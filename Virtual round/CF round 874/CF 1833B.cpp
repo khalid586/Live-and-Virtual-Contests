3c4ac3c6001f35c10c360959e0a7b3b6710d6ec0
khalid <khalidcomilla58@gmail.com>
2023-06-10 16:32:02 +0600

    updated

--- Virtual round/CF round 874/CF 1833B.cpp
+++ Virtual round/CF round 874/CF 1833B.cpp
@@ -15,31 +15,7 @@ using namespace std;
 #define endl            '\n'
 #define lcm(a,b)        (a*b)/(__gcd(a,b))
 #define lcm3(a,b,c)     (lcm(a,b)*c)/(__gcd(c,lcm(a,b)))
-//----------------------------------------------------debugger-------------------------------------------------------------// |
-void _print(ll t) {cerr << t <<",";}
-void _print(int t) {cerr << t<<",";}
-void _print(string t) {cerr << t<<",";}
-void _print(char t) {cerr << t<<",";}
-void _print(lld t) {cerr << t<<",";}
-void _print(double t) {cerr << t<<",";}
-void _print(ull t) {cerr << t<<",";}
 
-template <class T, class V> void _print(pair <T, V> p);
-template <class T> void _print(vector <T> v);
-template <class T> void _print(set <T> v);
-template <class T, class V> void _print(map <T, V> v);
-template <class T> void _print(multiset <T> v);
-template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}\n";}
-template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]\n";}
-template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]\n";}
-template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]\n";}
-template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]\n";}
-//----------------------------------------------------debugger-------------------------------------------------------------//|
-template<class T>
-using indexed_set = tree<T,null_type,less<T>,rb_tree_tag,
-tree_order_statistics_node_update>;
-// *s.find_by_order returns the value stored at that position
-// s.order_of_key returns the position where the value is stored
 //Starting in the name of Allah(THE ALMIGHTY)
 
 #define rep(i,a,b)      for(ll i = a; i <  b;++i)
@@ -57,16 +33,6 @@ void FastIO()
 
     #endif // ONLINE_JUDGE      
 }
-/*
-  1694B
-paranoid string hocche emon string ja 01 ---> 1 ebong 10 ----> 0 te
-convert kore shei string ke 1 length er string e convert kora possible
-
-amake ekta string deya thakte amake bolte hobe emon koyta 
-substring ache oi string er moddhe jara paranoid
-
-ans will be atleast  n 
-*/
 
 // #define int long long int
 void solve(int t){
