3e86c545954c6f503e6a1ac38c1bbdd3bd0b2a54
khalid <khalidcomilla58@gmail.com>
2023-09-07 22:49:23 +0600

    solution of round 895 added

---
+++ LIve Contests/CF Round 895/CF 1872C.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+
+#define ll              long long 
+#define khalid          std::ios::sync_with_stdio(false);cin.tie(NULL);
+#define asc_sort(v)     sort(v.begin(), v.end())
+#define dsc_sort(v)     sort(v.rbegin(), v.rend())
+#define sum(v)          accumulate(v.begin(),v.end(),0ll)
+#define lld             long double
+#define ull             unsigned long long
+#define all(s)          s.begin(),s.end()
+#define endl            '\n'
+#define lcm(a,b)        (a*b)/(__gcd(a,b))
+#define lcm3(a,b,c)     (lcm(a,b)*c)/(__gcd(c,lcm(a,b)))
+//----------------------------------------------------debugger-------------------------------------------------------------// |
+void _print(ll t) {cerr << t <<",";}
+void _print(int t) {cerr << t<<",";}
+void _print(string t) {cerr << t<<",";}
+void _print(char t) {cerr << t<<",";}
+void _print(lld t) {cerr << t<<",";}
+void _print(double t) {cerr << t<<",";}
+void _print(ull t) {cerr << t<<",";}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}\n";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]\n";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]\n";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]\n";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]\n";}
+//----------------------------------------------------debugger-------------------------------------------------------------//|
+template<class T>
+using indexed_set = tree<T,null_type,less<T>,rb_tree_tag,
+tree_order_statistics_node_update>;
+// *s.find_by_order returns the value stored at that position
+// s.order_of_key returns the position where the value is stored
+//Starting in the name of Allah(THE ALMIGHTY)
+
+#define rep(i,a,b)      for(ll i = a; i <  b;++i)
+#define rev(i,a,b)      for(ll i = a; i >= b;--i)
+
+
+//Starting in the name of Allah(THE ALMIGHTY)
+void FastIO()
+{
+     khalid
+     #ifndef ONLINE_JUDGE
+          freopen("in.txt","r", stdin);
+          //freopen("out.txt", "w", stdout);
+          freopen("Error.txt", "w", stderr);
+     #endif // ONLINE_JUDGE      
+}
+
+#define int long long int
+const int N = 1e6+7;
+
+pair<int,int> prime(int n){
+     if(n == 1) return {0,0};
+
+     for(int i = 2 ; i*i <= n; i++) 
+          if(!(n%i)) return {i,n/i};
+
+     return {0,0};
+}
+
+void solve(int t){
+     int a,b; cin >> a >> b;
+
+     rep(i,a,b+1){
+          pair<int,int> res = prime(i);
+          
+          int d = res.first , q = res.second;
+
+          if(d != 0 and q != 0){
+               cout << q << ' ' << (d-1)*q << '\n'; return;
+          }
+     }
+
+     cout << -1 << '\n';
+}
+
+int32_t main()
+{
+     FastIO();
+
+     ll T = 1;
+     cin >> T;
+
+     rep(i,1,T+1){
+          solve(i);
+     }
+     return 0;
+}
+
